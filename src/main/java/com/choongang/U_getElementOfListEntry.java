32dfad2121612a9bd4dca463d85fa5f7ccddb49a
Jungmin Lee <75019459+Lucky-kor@users.noreply.github.com>
2024-04-22 11:27:14 +0900

    Update U_getElementOfListEntry.java

--- src/main/java/com/choongang/U_getElementOfListEntry.java
+++ src/main/java/com/choongang/U_getElementOfListEntry.java
@@ -4,13 +4,21 @@ import java.util.*;
 
 public class U_getElementOfListEntry {
     public String getElementOfListEntry(HashMap<String, List<String>> hashMap, String key, int index) {
-        if (!hashMap.containsKey(key)) {
-            return null;
+        // if (!hashMap.containsKey(key)) {
+        //     return null;
+        // }
+        // List<String> valueList = hashMap.get(key);
+        // if (index < 0 || index >= valueList.size()) {
+        //     return null;
+        // }
+        // return valueList.get(index);
+
+        if(hashMap.containsKey(key)) {
+            List<String> list = hashMap.get(key);
+            if(index >= 0 && index < list.size()) {
+                return list.get(index);
+            }
         }
-        List<String> valueList = hashMap.get(key);
-        if (index < 0 || index >= valueList.size()) {
-            return null;
-        }
-        return valueList.get(index);
+        return null;
     }
 }
